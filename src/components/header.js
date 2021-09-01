3130331a1a86b07ba97d90a82484adc71b9d7d7a
Andrew-Bray <amichaelbray@gmail.com>
2021-08-31 19:52:54 -0700

    mdule styling plus gatsby reset

--- src/components/header.js
+++ src/components/header.js
@@ -12,11 +12,23 @@ const Header = () => {
         </div>
         <div >
           <ul className={styles.navList}>
-            <li><Link className={styles.navItem} activeClassName={styles.activeNavItem} to='/'>Home</Link></li>
+            <li><Link 
+              className={styles.navItem} 
+              activeClassName={styles.activeNavItem} 
+              to='/'>Home</Link></li>
           
-          <li><Link className={styles.navItem} activeClassName={styles.activeNavItem} to='/about'>About Me</Link></li>
-          <li><Link className={styles.navItem} activeClassName={styles.activeNavItem} to='/contact'>Contact</Link></li>
-          <li><Link className={styles.navItem} activeClassName={styles.activeNavItem} to='/blog'>Blog</Link></li>
+            <li><Link 
+              className={styles.navItem} 
+              activeClassName={styles.activeNavItem} 
+              to='/about'>About Me</Link></li>
+            <li><Link 
+              className={styles.navItem} 
+              activeClassName={styles.activeNavItem} 
+              to='/contact'>Contact</Link></li>
+            <li><Link 
+              className={styles.navItem} 
+              activeClassName={styles.activeNavItem} 
+              to='/blog'>Blog</Link></li>
         </ul>
         </div>
       </header>

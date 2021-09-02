7484e07327a578eddd0bfcd94014fd19aefb3f7b
Andrew-Bray <amichaelbray@gmail.com>
2021-09-02 11:13:11 -0700

    adjusting the index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ export default function Home() {
   return (
     <Layout>
       <Head title="Home"/>
-      <h1>Hello world!</h1>
+      <h1>Hello all!</h1>
+      <h2>I'm Andrew Bray, a software developer, theatre educator, sound artist, and beekeeper</h2>
     </Layout>
     )
 }

fd7b2d34d173423f9facba87b54049ab4dcf0aa2
Andrew-Bray <amichaelbray@gmail.com>
2021-09-02 18:04:53 -0700

    final styling and updated blogs

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
+import { Link } from 'gatsby'
 
 import * as footerStyles from './footer.module.scss'
 
@@ -18,6 +19,11 @@ console.log(data);
   return (
     <footer className={footerStyles.footer}>
       <p>Proudly created with Gatsby by {data.site.siteMetadata.author} © 2021</p>
+      <p style={{ fontSize: ".7rem"}}>Visit the 
+        <Link to="https://github.com/Andrew-Bray/gatsby-contentful-tutorial"
+              target="_blank" rel="noopener noreferrer"
+      > Source Code </Link>
+      for this website</p>
     </footer>
   )
 }
